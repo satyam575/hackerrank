994e40ddb200a998bb6188be7292f2edd1ff7854
Steve Bosilovatz <bosilovatz@gmail.com>
2017-08-25 16:48:01 -0500

    Staircase solution

---
+++ staircase.js
@@ -0,0 +1,12 @@
+ for(var i = 0; i < n; i++){
+     var s = "";
+        for(var j = 0; j < n; j++){
+            if(n - i - 2 < j){
+                s += "#";
+		} else {
+                s += " ";
+            }
+        }
+        console.log(s);
+    };
+	
