97cc5d5a8d93ba840a38dd9c77a7df0a4c802bf0
Steve Bosilovatz <bosilovatz@gmail.com>
2017-10-31 19:10:29 -0500

    The Hurdle Race in Java

---
+++ hurdles.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        for(int height_i=0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+         Arrays.sort(height);
+         int largest = height[height.length - 1];
+        
+         if (largest > k) {
+            System.out.println(largest - k);
+         } else {
+            System.out.println(0);
+         }
+    }
+}
