3d9281207f33d869b3717d0b7cf5a5c592193aac
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-11 06:29:37 -0600

    Updated solution

--- averybigsum.js
+++ averybigsum.js
@@ -1,6 +1,12 @@
+// original solution
+
 function aVeryBigSum(n, ar) {
     var sum = ar.reduce(function(a,b) {
        return a + b;
     })
     return sum                    
 }
+
+// updated solution with es6 syntax
+
+const aVeryBigSum = (n, ar) => ar.reduce(function(a,b) { return a + b; })
