7814784a05968b78be71e8e4342006a65e8c4f1b
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-11 06:41:38 -0600

    Updated solution

--- staircase.js
+++ staircase.js
@@ -1,3 +1,5 @@
+ // original solution
+ 
  for(var i = 0; i < n; i++){
      var s = "";
         for(var j = 0; j < n; j++){
@@ -10,3 +12,16 @@
         console.log(s);
     };
 	
+// updated solution
+
+const main = () => {
+    var n = parseInt(readLine());
+    for (i = 0; i < n; i++) {
+        let s = ""
+        for (j = 0; j < n; j++) {
+            if (n - i - 2 < j) s += '#'
+            else s += " "
+        }
+    console.log(s)
+    }
+}
