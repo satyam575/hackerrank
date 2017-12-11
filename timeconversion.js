e3413bff8927f17f342890f0a6bca733c3a61785
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-11 07:07:10 -0600

    Updated solution

--- timeconversion.js
+++ timeconversion.js
@@ -1,3 +1,5 @@
+   // original solution
+   
    var hour = time.substring(0,2);
    var after = time.substring(2,8);
    var m = time.substring(8,11);
@@ -25,3 +27,34 @@
    }
  
     console.log(hour + after);
+	
+// updated solution with es6 syntax
+
+const timeConversion = s => {
+    
+    let hour = s.substring(0,2);
+    let after = s.substring(2,8);
+    let am_pm = s.substring(8,11);
+    
+    if (am_pm == "PM")
+        switch(hour) {
+            case "01": hour = "13"; break
+            case "02": hour = "14"; break
+            case "03": hour = "15"; break
+            case "04": hour = "16"; break
+            case "05": hour = "17"; break
+            case "06": hour = "18"; break
+            case "07": hour = "19"; break
+            case "08": hour = "20"; break
+            case "09": hour = "21"; break
+            case "10": hour = "22"; break
+            case "11": hour = "23"; break       
+         }  
+    
+    if (am_pm == "AM") 
+        switch(hour) {
+            case "12": hour = "00"; break
+        }
+    
+    return hour + after
+}
