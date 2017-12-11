d823a97bbe6a4501cd133279fac0d9d1d7772498
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-11 06:14:26 -0600

    Updated solution

--- simplesumarray.js
+++ simplesumarray.js
@@ -1,4 +1,25 @@
+/*
+
+Given an array of N integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, N , denoting the size of the array. 
+The second line contains N space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+*/
+
+// original solution
+
 function simpleArraySum(n, ar) {
    var sum = ar.reduce(function(a, b) { return a + b; }, 0);
     console.log(sum);
 }
+
+// updated solution with es6 syntax
+
+const simpleArraySum = (n, ar) => ar.reduce(function(a, b) { return a + b; }, 0);
