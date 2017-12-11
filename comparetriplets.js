a984afe7da057bd283c87cabd160441f3d570c1b
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-11 06:25:52 -0600

    Updated solution

--- comparetriplets.js
+++ comparetriplets.js
@@ -1,3 +1,5 @@
+// original solution
+
 function solve(a0, a1, a2, b0, b1, b2){
     var alice = 0;
     var bob = 0;
@@ -19,4 +21,18 @@ function solve(a0, a1, a2, b0, b1, b2){
     
     console.log(alice + " " + bob)
     
+}
+
+// updated solution with es6 syntax
+
+const solve = (a0, a1, a2, b0, b1, b2) => {
+    let alice = 0;
+    let bob = 0;
+    if (a0 > b0 === true)  alice++
+    if (b0 > a0 === true)  bob++
+    if (a1 > b1 === true)  alice++
+    if (b1 > a1 === true)  bob++
+    if (a2 > b2 === true)  alice++
+    if (b2 > a2 === true)  bob++
+    console.log(`${alice} ${bob}`)
 }
