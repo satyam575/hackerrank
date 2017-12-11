ca2df953afb9755a7fce2685274fa17c9f558aa7
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-11 06:35:12 -0600

    Updated solution

--- plusminus.js
+++ plusminus.js
@@ -1,6 +1,19 @@
-    var positive = arr.filter(function(x){ return x > 0 });
+    // original solution
+	
+	var positive = arr.filter(function(x){ return x > 0 });
     console.log(positive.length/n) 
     var negative = arr.filter(function(x){ return x < 0 });
     console.log(negative.length/n) 
     var neutral = arr.filter(function(x){ return x == 0 });
     console.log(neutral.length/n) 
+	
+	//updated solution
+	
+const main = () => {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    console.log(arr.filter(function(x){ return x > 0}).length/n)
+    console.log(arr.filter(function(x){ return x < 0}).length/n)
+    console.log(arr.filter(function(x){ return x == 0}).length/n)
+}
