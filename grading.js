aa4b7ea26d02688281decf51b6cef8ea3e85a714
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-12 13:01:20 -0600

    Updated solution

--- grading.js
+++ grading.js
@@ -1,3 +1,5 @@
+// original solution
+
 function solve(grades){
     for (i = 0; i < grades.length; i++) {
     var roundUp = Math.ceil(grades[i]/5)*5; 
@@ -11,3 +13,14 @@ function solve(grades){
         console.log(grades[i]);
     }  
 }    
+
+//updated solution with es6 syntax
+
+const solve = grades => {
+    for (let i of grades) {
+        let roundUp = Math.ceil(i/5)*5; 
+        if (i <= 37) i = i;
+        else if (roundUp - i < 3) i = roundUp  
+        console.log(i);
+    }        
+ }   
