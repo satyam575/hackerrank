83bd485f4a2a8ba4445af5788b818f50b94cb6e0
Steve Bosilovatz <bosilovatz@gmail.com>
2017-12-12 13:07:29 -0600

    Updated solution

--- appleoranges.js
+++ appleoranges.js
@@ -1,4 +1,6 @@
-    var appleCount = 0;
+    // original solution
+	
+	var appleCount = 0;
     var orangeCount = 0;
     
     for (i = 0; i < apple.length; i++) {
@@ -15,5 +17,20 @@
         }
     }
     
+    console.log(appleCount);
+    console.log(orangeCount);
+	
+	// updated solution with es6 syntax
+	
+	let appleCount = 0;
+    let orangeCount = 0; 
+    for (let i of apple) {
+       let posA = i + a;
+       if (posA >= s && posA <= t) appleCount++
+    }
+    for (let i of orange) {
+        let posO = i + b;
+        if (posO >= s && posO <= t) orangeCount++;    
+    }
     console.log(appleCount);
     console.log(orangeCount);
